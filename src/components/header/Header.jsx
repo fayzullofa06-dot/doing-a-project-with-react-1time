e254bb7273159af4e29f4ef11a86b0f7bd5e2ca7
Habibullo09 <fayzullo.fa06@gmail.com>
2026-03-13 16:40:09 +0500

    i have done the half page of the first page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import"./Header.css";
-function Header() {
+import React, { useState } from 'react';
+import './Header.css';
+import logo from '../../assets/hero.png';
+
+
+export default function Header() {
+  const [active, setActive] = useState('travel');
+  const navbar = ['Home', 'Explore', 'Travel', 'Blog', 'Pricing'];
+
   return (
-    <header>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-       <a href="/">About</a>
-        </li>
-        <li>
-            <a href="Courses">Courses</a>
-        </li>
-        <li>
-            <a href="/">Contact</a>
-        </li>
-    </ul>
-      
+    <header className='header'>
+      <div className='logo'>
+        <img src={logo} alt="Logo"/>
+    
+      </div>
+
+ <ul className='nav-links'>
+  {navbar.map((item) => (
+    <li
+      key={item}
+      onClick={() => setActive(item)} 
+      className={active === item ? 'active' : ''}
+    >
+      {item}
+    </li>
+  ))}
+</ul>
+
+      <div className='btns'>
+        <button className='Log-btn'>Login</button>
+        <button className='Sign-btn'>Sign up</button>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './hero.css' 
+import heroImg from '../../assets/hero.jpg'
+export default function Hero() {
+  return (
+    <>
+    <div className='section'>
+          <div className='hero-section'>
+           <p className='hero-text'>Start your unforgettable <br />journey with us.</p> 
+           <p className='hero-travel'>The best travel for your jouney begins now</p>
+          </div>
+    </div>
+    
+    </>
+  )
+}
