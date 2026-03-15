53d14d5144b2990e5ddd35fe5d8d596f7a76e896
Habibullo09 <fayzullo.fa06@gmail.com>
2026-03-15 16:16:41 +0500

    i have added booking part

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <>
     <div className='section'>
           <div className='hero-section'>
-           <p className='hero-text'>Start your unforgettable <br />journey with us.</p> 
+           <p className='hero-text'>Start your unforgettable  <br />journey with us</p> 
            <p className='hero-travel'>The best travel for your jouney begins now</p>
           </div>
     </div>
